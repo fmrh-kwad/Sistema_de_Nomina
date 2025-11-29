079095de5d5a553031eb8340d2e6a892f225cb76
lya <ferrerasmarielya@gmail.com>
2025-11-28 21:38:20 -0500

    menu creado
    funciones necesarias mencionadas en el menu; aun faltan por crearse
    
    funcion de 'solicitar numero' creada
    
    funcion de registro de empleados creada
    
    extra: uso de bold text y italic para textos por razones esteticas

--- main.py
+++ main.py
@@ -13,6 +13,50 @@ n = 'número'
 
 #   ------------------------------------
 
+ # funcion para menu simple
+  #nota: los elif's y textos con "#" no han sido funciones creadas en el programa todavia. una vez creadas
+  # se le quitaran los hashtags.
+
+def menu():
+    empleados = {}
+    opciones = {
+        "1": "Registrar empleado",
+        # "2": "Actualizar salario",
+        # "3": "Registrar horas",
+        # "4": "Calcular pago",
+        # "5": "Reporte: salario más alto",
+        # "6": "Reporte: promedio salarial",
+        # "7": "Reporte: empleados con horas extra",
+        "0": "Salir"
+    }
+
+    # bucle infinito hasta que se eliga una opcion del menu
+    while True:
+        print("\n=== MENÚ ===")
+        for k, v in opciones.items():
+            print(f"{k}. {v}")
+        op = input("Elija una opción: ").strip()
+
+        if op == "1":
+            empleados, _ = registro_empleado(empleados)
+        #elif op == "2":
+            empleados, _ = actualizar_salario(empleados)
+        #elif op == "3":
+            empleados, _ = registrar_horas(empleados)
+        #elif op == "4":
+            empleados, _ = calcular_pago(empleados)
+        #elif op == "5":
+            _ = reporte_salario_mas_alto(empleados)
+        #elif op == "6":
+            _ = reporte_promedio_salarial(empleados)
+        #elif op == "7":
+            _ = reporte_empleados_con_horas_extras(empleados)
+        elif op == "0":
+            print("Saliendo...")
+            break
+        else:
+            print("Opción inválida. Intente nuevamente.")
+
 
 
  #L: funcion para evitar que una vez el usuario introduzca un caracter que no sea igual a un numero en
@@ -40,7 +84,12 @@ def registro_empleado(empleados):
 
     if id_emp in empleados:
         print("Error: Ya existe un empleado con ese ID.")
-        return empleados, None   # devuelve diccionario sin cambios y None
+        return empleados, None   # devuelve diccionario sin cambios --None
+
+    if not nombre:
+        print("Error: El nombre no puede estar vacío.")
+        return empleados, None
+        #no permite que se registre un empleado sin nombre/datos vacios al presionar 'enter'.
 
     nombre = input("Ingrese el nombre del empleado: ").strip()
     salario_hora = solicitar_numero("Ingrese el salario por hora: ")
